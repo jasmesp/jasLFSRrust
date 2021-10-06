9d0ff0acaa9832ecaafd540cf2a3731babc89f61
jasmesp <jasmes@gmail.com>
2021-10-06 19:59:12 -0400

    fix

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ fn  math(arg: i128){
     let mut iv: i128 = (1 >> 127) | 1;
     for _i in 1..arg{
         let s_bit = (iv ^ (iv >> 1) ^ (iv >> 2) ^ (iv >> 7));
-        iv = (iv ^ (s_bit >> 1) ^ (s_bit >> 2) ^ (s_bit >> 7));
-        iv = (s_bit >> 1) | (iv << 3);
+        shiftval = (iv ^ (s_bit >> 1) ^ (s_bit >> 2) ^ (s_bit >> 7));
+        iv = (iv >> 1) | (shiftval << 127);
         print!("{:b}", iv);
         //output(iv);
     }
