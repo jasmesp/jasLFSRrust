dd18e236877d3916757bca43ee682e07463cea76
Jas <jasmes@gmail.com>
2021-10-06 17:56:04 -0400

    tweaks

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+
+
+fn output(arg: i128) {
+    //define a binary value
+    let bv = arg;
+    println!("{:b}", bv);    
+}
+
+fn  math(arg: i128){
+    
+    let mut iv: i128 = (1 >> 127) | 1;
+    for _i in 1..arg{
+        let s_bit = (iv ^ (iv >> 1) ^ (iv >> 2) ^ (iv >> 7));
+        iv = (iv ^ (s_bit >> 1) ^ (s_bit >> 2) ^ (s_bit >> 7));
+        iv = (s_bit >> 1) | (iv << 3);
+        print!("{:b}", iv);
+        //output(iv);
+    }
+}
+
+
+
+
+
+fn main() {
+    println!("Hello, sex!\n");
+    math(12*12);    
+    print!("\n\nsexxx");
+    println!("\nmoresexxx\n");
+    output(0b1100);
+}
