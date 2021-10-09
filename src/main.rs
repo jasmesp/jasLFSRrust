a3eec1a044764f3e66da649789870990ac7ff882
Jas Palermo <jasmes@gmail.com>
2021-10-09 11:58:16 -0400

    work in prog

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn  math(arg: i128){
     let mut iv: i128 = (1 >> 127) | 1;
     for _i in 1..arg{
         let s_bit = (iv ^ (iv >> 1) ^ (iv >> 2) ^ (iv >> 7));
-        shiftval = (iv ^ (s_bit >> 1) ^ (s_bit >> 2) ^ (s_bit >> 7));
+        let shiftval = (iv ^ (s_bit >> 1) ^ (s_bit >> 2) ^ (s_bit >> 7));
         iv = (iv >> 1) | (shiftval << 127);
         print!("{:b}", iv);
         //output(iv);
@@ -24,7 +24,7 @@ fn  math(arg: i128){
 
 fn main() {
     println!("Hello, sex!\n");
-    math(12*12);    
+    math(12*24);    
     print!("\n\nsexxx");
     println!("\nmoresexxx\n");
     output(0b1100);
